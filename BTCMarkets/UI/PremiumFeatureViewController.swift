5f61c90e117df6258b244baf58b4a8e023a80ce3
Stephen Yao <stephen.yao@rea-group.com>
2018-01-24 08:26:01 +1100

    fetch the in app purchase

--- BTCMarkets/UI/PremiumFeatureViewController.swift
+++ BTCMarkets/UI/PremiumFeatureViewController.swift
@@ -7,9 +7,66 @@
 //
 
 import UIKit
+import StoreKit
+
+private let productIdentifier = "com.btcmarkets.pro.version"
+
+final class InAppPurchase: NSObject {
+
+  typealias ProductRequestCompletion = (SKProduct) -> Void
+
+  var productsRequest: SKProductsRequest?
+  var product: SKProduct?
+  var productRequestCompletion: ProductRequestCompletion?
+
+  override init() {
+    super.init()
+    SKPaymentQueue.default().add(self)
+  }
+}
+
+extension InAppPurchase: SKProductsRequestDelegate {
+  func fetchProducts(completion: @escaping ProductRequestCompletion) {
+    productsRequest?.cancel()
+
+    productsRequest = SKProductsRequest(productIdentifiers: Set([productIdentifier]))
+    productsRequest!.delegate = self
+    productsRequest!.start()
+
+    self.productRequestCompletion = completion
+  }
+
+  func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+    guard let product = response.products.first else { return }
+    self.product = product
+    productRequestCompletion?(product)
+  }
+}
+
+extension InAppPurchase: SKPaymentTransactionObserver {
+  func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+    for transaction in transactions {
+      switch (transaction.transactionState) {
+      case .purchased:
+        print("purchased")
+      case .failed:
+        print("fail")
+      case .restored:
+        print("restored")
+      case .deferred:
+        print("deferred")
+      case .purchasing:
+        print("purchasing")
+      }
+    }
+  }
+}
 
 class PremiumFeatureViewController: UIViewController {
   static let storyboardName = "PremiumFeature"
+  @IBOutlet var purchaseButton: UIButton!
+
+  let inAppPurchase: InAppPurchase = InAppPurchase()
 
   @IBAction func laterButtonTapped(_ sender: Any) {
     if let parent = self.parent as? PremiumFeature {
@@ -17,10 +74,15 @@ class PremiumFeatureViewController: UIViewController {
     }
   }
 
-  @IBAction func purchaseButtonTapped(_ sender: Any) {    
+  @IBAction func purchaseButtonTapped(_ sender: Any) {
+    
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    inAppPurchase.fetchProducts() { [weak self] in
+      self?.purchaseButton.setTitle("Buy \($0.localizedTitle) for \($0.price.floatValue) per year", for: .normal)
+    }
   }
 }
