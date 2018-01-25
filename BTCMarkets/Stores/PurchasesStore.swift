c68f6255e8d11e75e36fc18adb701cf1f2cb668d
Stephen Yao <stephenryao@gmail.com>
2018-01-26 03:18:34 +1100

    Modify copy

--- BTCMarkets/Stores/PurchasesStore.swift
+++ BTCMarkets/Stores/PurchasesStore.swift
@@ -153,7 +153,6 @@ extension InAppPurchase: SKPaymentTransactionObserver {
       case .failed:
         SKPaymentQueue.default().finishTransaction(transaction)
         print("failed")
-        return
       case .deferred:
         return
       case .purchasing:
