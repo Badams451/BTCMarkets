ef49a2954061cfcc241b39aa9d3e9d091a297e7a
Stephen Yao <stephenryao@gmail.com>
2018-01-26 11:26:51 +1100

    Changed default ticker name

--- BTCMarkets/Stores/TickerStore.swift
+++ BTCMarkets/Stores/TickerStore.swift
@@ -19,7 +19,7 @@ class TickerStore {
   static var sharedInstance: TickerStore = TickerStore()
   private var tickerSubscribers: [(Subscriber, TickersChanged)] = []
   private var selectedTickerSubscribers: [(Subscriber, SelectedTickerChanged)] = []
-  let defaultTicker = Ticker(tickerName: "BTC Markets", currency: .aud, instruments: [.btc, .ltc, .xrp, .eth, .bch])
+  let defaultTicker = Ticker(tickerName: "Ticker", currency: .aud, instruments: [.btc, .ltc, .xrp, .eth, .bch])
   
   init() {
     retrieveTickers()
