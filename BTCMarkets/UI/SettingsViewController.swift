a109de5cdd3aeca73c3160053e1dd3a7cc23d9cc
Stephen Yao <stephen.yao@rea-group.com>
2018-06-08 20:02:38 -0700

    Added SettingsViewController

---
+++ BTCMarkets/UI/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  BTCMarkets
+//
+//  Created by Stephen Yao on 6/8/18.
+//  Copyright © 2018 Stephen Yao. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+  
+}
